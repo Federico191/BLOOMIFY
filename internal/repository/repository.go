1614dd28e2ab45f8da1826b09fc19d5608697775
Federico191 <federicoberto708@gmail.com>
2024-03-08 03:09:01 +0700

    modify repository.go

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,9 +3,19 @@ package repository
 import "gorm.io/gorm"
 
 type Repository struct {
-	User UserRepoItf
+	User          UserRepoItf
+	BeautyClinic  BeautyClinicRepoItf
+	SalonRepo     SalonRepoItf
+	SpaMassage    SpaMassageRepoItf
+	FitnessCenter FitnessCenterRepoItf
 }
 
 func Init(db *gorm.DB) *Repository {
-	return &Repository{User: NewUserRepo(db)}
+	return &Repository{
+		User:          NewUserRepo(db),
+		BeautyClinic:  NewBeautyClinicRepo(db),
+		SalonRepo:     NewSalonRepo(db),
+		SpaMassage:    NewSpaMassageRepo(db),
+		FitnessCenter: NewFitnessCenterRepo(db),
+	}
 }
