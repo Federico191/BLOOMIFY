49bb7295819bb7b3b3281bf37356402377fb8a56
Federico191 <federicoberto708@gmail.com>
2024-03-08 03:09:36 +0700

    modify salon.go

--- internal/entity/salon.go
+++ internal/entity/salon.go
@@ -7,13 +7,22 @@ import (
 
 type Salon struct {
 	ID        uuid.UUID `gorm:"primaryKey;size:36"`
-	Name      string    `gorm:"size:255;not null;unique"`
+	Name      string    `gorm:"size:255;not null"`
 	Address   string    `gorm:"size:255;not null;unique"`
 	City      string    `gorm:"size:255;not null"`
 	Contact   string    `gorm:"size:100;not null"`
-	Rating    float64   `gorm:"not null"`
-	Hour      uint      `gorm:"not null"`
+	Hour      string    `gorm:"size:20;not null"`
 	PhotoLink string    `gorm:"size:200"`
 	CreatedAt time.Time `gorm:"autoCreateTime:milli"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime:milli"`
+}
+
+type SalonTreatment struct {
+	SalonId   uuid.UUID `gorm:"primaryKey;varchar(36)"`
+	Salon     Salon     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Name      string    `gorm:"size:255;not null"`
+	Problem   string    `gorm:"size:255;not null"`
+	Price     uint      `gorm:"not null"`
+	CreatedAt time.Time `gorm:"autoUpdateTime:milli"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime:milli"`
 }
