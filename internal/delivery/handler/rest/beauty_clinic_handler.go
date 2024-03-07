3a890d2902b43f8e744f290be178749fb6115ced
Federico191 <federicoberto708@gmail.com>
2024-03-08 03:07:44 +0700

    add beauty_clinic, fitness_center, salon, spa_massage handler

---
+++ internal/delivery/handler/rest/beauty_clinic_handler.go
@@ -0,0 +1,58 @@
+package rest
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"projectIntern/internal/model"
+	"projectIntern/internal/usecase"
+	"projectIntern/pkg/response"
+	"strconv"
+)
+
+type BeautyClinicHandler struct {
+	useCase usecase.BeautyClinicUCItf
+}
+
+func NewBeautyClinicHandler(useCase usecase.BeautyClinicUCItf) *BeautyClinicHandler {
+	return &BeautyClinicHandler{useCase: useCase}
+}
+
+func (bc BeautyClinicHandler) Create(ctx *gin.Context) {
+	var req model.BeautyClinicRequest
+
+	if err := ctx.ShouldBindJSON(req); err != nil {
+		response.Error(ctx, http.StatusBadRequest, "failed to bind request", err)
+		return
+	}
+
+	beautyClinic, err := bc.useCase.Create(req)
+	if err != nil {
+		response.Error(ctx, http.StatusInternalServerError, "failed to create to beauty clinic", err)
+		return
+	}
+
+	response.Success(ctx, http.StatusOK, "successfully create beauty clinic", beautyClinic)
+}
+
+func (bc BeautyClinicHandler) GetByCity(ctx *gin.Context) {
+	pageQuery := ctx.Query("page")
+	city := ctx.Query("city")
+
+	page, err := strconv.Atoi(pageQuery)
+	if err != nil {
+		response.Error(ctx, http.StatusUnprocessableEntity, "failed to bind request", err)
+	}
+
+	beautyClinics, err := bc.useCase.GetByCity(city, page)
+	if err != nil {
+		response.Error(ctx, http.StatusInternalServerError, "failed to get beauty clinic by city", err)
+	}
+
+	if len(beautyClinics) == 0 {
+		response.Error(ctx, http.StatusNotFound, "record not found", errors.New(""))
+		return
+	}
+
+	response.Success(ctx, http.StatusOK, "successfully get beauty clinics by city", beautyClinics)
+}
