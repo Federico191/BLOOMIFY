4a4b145fc7693bd947dc237fc1f8d43878964643
Federico191 <federicoberto708@gmail.com>
2024-03-08 03:10:43 +0700

    modify usecase.go

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -6,11 +6,21 @@ import (
 )
 
 type UseCase struct {
-	Auth AuthUseCaseItf
+	Auth          AuthUseCaseItf
+	User          UserUCItf
+	BeautyClinic  BeautyClinicUCItf
+	Salon         SalonUCItf
+	SpaMassage    SpaMassageUCItf
+	FitnessCenter FitnessCenterUCItf
 }
 
 func Init(repo *repository.Repository, tokenMaker jwt.JWTMakerItf) *UseCase {
 	return &UseCase{
-		Auth: NewAuthUseCase(repo.User, tokenMaker),
+		Auth:          NewAuthUseCase(repo.User, tokenMaker),
+		BeautyClinic:  NewBeautyClinicUC(repo.BeautyClinic),
+		Salon:         NewSalonUC(repo.SalonRepo),
+		SpaMassage:    NewSpaMassageUC(repo.SpaMassage),
+		User:          NewUseUC(repo.User),
+		FitnessCenter: NewFitnessCenterUc(repo.FitnessCenter),
 	}
 }
