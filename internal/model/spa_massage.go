091951b67ec06bb0ebdce3674eb069179daf09e0
Federico191 <federicoberto708@gmail.com>
2024-03-08 03:04:03 +0700

    add fitness_center, salon, spa_massage model

---
+++ internal/model/spa_massage.go
@@ -0,0 +1,40 @@
+package model
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type SpaMassageRequest struct {
+	Name      string  `json:"name" binding:"required"`
+	Address   string  `json:"address" binding:"required"`
+	City      string  `json:"city" binding:"required"`
+	Contact   string  `json:"contact" binding:"required"`
+	Rating    float64 `json:"rating" binding:"required;numeric"`
+	Hour      string  `json:"hour" binding:"required"`
+	PhotoLink string  `json:"photo_link"`
+}
+
+type SpaMassageUpdate struct {
+	ID        uuid.UUID `json:"id" binding:"required;uuid"`
+	Name      string    `json:"name" binding:"required"`
+	Address   string    `json:"address" binding:"required"`
+	City      string    `json:"city" binding:"required"`
+	Contact   string    `json:"contact" binding:"required"`
+	Rating    float64   `json:"rating" binding:"required;numeric"`
+	Hour      string    `json:"hour" binding:"required"`
+	PhotoLink string    `json:"photo_link"`
+}
+
+type SpaMassageResponse struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Address   string    `json:"address"`
+	City      string    `json:"city"`
+	Contact   string    `json:"contact"`
+	Rating    float64   `json:"rating"`
+	Hour      string    `json:"hour"`
+	PhotoLink string    `json:"photo_link"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
