8ed52aab3fafd8eaf2464475ae158aea37aa4a26
Federico191 <federicoberto708@gmail.com>
2024-03-09 23:07:03 +0700

    modify migration.go

--- pkg/database/mysql/migration.go
+++ pkg/database/mysql/migration.go
@@ -9,19 +9,12 @@ import (
 func Migration(db *gorm.DB) {
 	err := db.Migrator().DropTable(
 		entity.User{},
-		entity.BeautyClinic{},
-		entity.Salon{},
-		entity.SpaMassage{},
-		entity.FitnessCenter{})
+	)
 	if err != nil {
 		log.Fatalf("failed drop table: %v", err)
 	}
 
-	err = db.AutoMigrate(entity.User{},
-		entity.BeautyClinic{},
-		entity.Salon{},
-		entity.SpaMassage{},
-		entity.FitnessCenter{})
+	err = db.AutoMigrate(entity.User{})
 	if err != nil {
 		log.Fatalf("failed to migrate : %v", err)
 	}
