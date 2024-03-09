6dfe56962d8ae719c4e7e4ffde0762cf8ca2395e
Federico191 <federicoberto708@gmail.com>
2024-03-09 23:05:37 +0700

    add verificationCode atribut

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -6,12 +6,13 @@ import (
 )
 
 type User struct {
-	ID         uuid.UUID `gorm:"primaryKey;varchar(36)"`
-	Email      string    `gorm:"size:50;not null;unique"`
-	FullName   string    `gorm:"size:255; not null"`
-	Avatar     string    `gorm:"size:255"`
-	Password   string    `gorm:"size:100"`
-	IsVerified bool      `gorm:"default:false"`
-	CreatedAt  time.Time `gorm:"autoCreateTime:milli"`
-	UpdatedAt  time.Time `gorm:"autoCreateTime:milli;autoUpdateTime:milli"`
+	ID               uuid.UUID `gorm:"primaryKey;varchar(36)"`
+	Email            string    `gorm:"size:50;not null;unique"`
+	FullName         string    `gorm:"size:255; not null"`
+	PhotoLink        string    `gorm:"size:255"`
+	Password         string    `gorm:"size:100;not null"`
+	VerificationCode string    `gorm:"size:30"`
+	IsVerified       bool      `gorm:"default:false"`
+	CreatedAt        time.Time `gorm:"autoCreateTime:milli"`
+	UpdatedAt        time.Time `gorm:"autoCreateTime:milli;autoUpdateTime:milli"`
 }
