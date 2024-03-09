3bc5f9a389393d3394a7b7ffc3ab8359ae1a3366
Federico191 <federicoberto708@gmail.com>
2024-03-09 23:02:56 +0700

    add function verifyEmail

--- internal/delivery/handler/rest/auth_handler.go
+++ internal/delivery/handler/rest/auth_handler.go
@@ -7,15 +7,16 @@ import (
 	"projectIntern/internal/model"
 	"projectIntern/internal/usecase"
 	"projectIntern/pkg/customerrors"
+	"projectIntern/pkg/encode"
 	"projectIntern/pkg/response"
 )
 
 type AuthHandler struct {
-	userUC usecase.AuthUseCaseItf
+	authUC usecase.AuthUseCaseItf
 }
 
-func NewAuthHandler(userUC usecase.AuthUseCaseItf) *AuthHandler {
-	return &AuthHandler{userUC: userUC}
+func NewAuthHandler(authUC usecase.AuthUseCaseItf) *AuthHandler {
+	return &AuthHandler{authUC: authUC}
 }
 
 func (a AuthHandler) Register(c *gin.Context) {
@@ -26,7 +27,7 @@ func (a AuthHandler) Register(c *gin.Context) {
 		return
 	}
 
-	user, err := a.userUC.Register(&req)
+	user, err := a.authUC.Register(&req)
 	if err != nil {
 		if errors.Is(err, customerrors.ErrEmailAlreadyExists) {
 			response.Error(c, http.StatusConflict, "failed to create user", err)
@@ -44,12 +45,16 @@ func (a AuthHandler) Login(c *gin.Context) {
 
 	if err := c.ShouldBind(&req); err != nil {
 		response.Error(c, http.StatusBadRequest, "Failed to bind request", err)
+		return
 	}
 
-	token, err := a.userUC.Login(&req)
+	token, err := a.authUC.Login(&req)
 	if err != nil {
 		if errors.Is(err, customerrors.ErrEmailInvalid) || errors.Is(err, customerrors.ErrPasswordInvalid) {
-			response.Error(c, http.StatusNotFound, "failed to log in", err)
+			response.Error(c, http.StatusUnauthorized, "failed to log in", err)
+			return
+		} else if errors.Is(err, customerrors.ErrNotVerified) {
+			response.Error(c, http.StatusForbidden, "failed to log in", err)
 			return
 		}
 		response.Error(c, http.StatusInternalServerError, "Failed to log in", err)
@@ -57,4 +62,33 @@ func (a AuthHandler) Login(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"Token": token, "Message": "Success Login"})
+}
+
+func (a AuthHandler) VerifyEmail(ctx *gin.Context) {
+	codeQuery := ctx.Param("code")
+
+	code, err := encode.Decode(codeQuery)
+	if err != nil {
+		response.Error(ctx, http.StatusUnprocessableEntity, "failed to bind request", err)
+		return
+	}
+
+	user, err := a.authUC.GetByVerificationCode(code)
+	if err != nil {
+		return
+	}
+
+	if user.VerificationCode != code {
+		response.Error(ctx, http.StatusBadRequest, "failed to verify", errors.New("verification code not same"))
+		return
+	}
+
+	user.IsVerified = true
+
+	err = a.authUC.VerifyEmail(user.ID)
+	if err != nil {
+		return
+	}
+
+	response.Success(ctx, http.StatusOK, "Successfully verification account", nil)
 }
