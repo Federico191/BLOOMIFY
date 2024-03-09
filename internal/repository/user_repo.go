4e7d94fc4df33d3de7bad3c6f49f835edd13e3da
Federico191 <federicoberto708@gmail.com>
2024-03-09 23:04:07 +0700

    add 2 function GetByVerificationCode and Update

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -1,15 +1,18 @@
 package repository
 
 import (
-	"errors"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"projectIntern/internal/entity"
+	"projectIntern/internal/model"
 )
 
 type UserRepoItf interface {
 	Create(user *entity.User) error
 	GetByEmail(email string) (*entity.User, error)
-	GetById(id string) (*entity.User, error)
+	GetById(id uuid.UUID) (*entity.User, error)
+	GetByVerificationCode(code string) (*entity.User, error)
+	Update(user *entity.User, req model.UserUpdate) error
 }
 
 type UserRepository struct {
@@ -34,16 +37,24 @@ func (u UserRepository) GetByEmail(email string) (*entity.User, error) {
 
 	err := u.db.Where("email = ?", email).First(&user).Error
 	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, err
-		}
 		return nil, err
 	}
 
 	return user, nil
 }
 
-func (u UserRepository) GetById(id string) (*entity.User, error) {
+func (u UserRepository) GetByVerificationCode(code string) (*entity.User, error) {
+	var user *entity.User
+
+	err := u.db.Debug().Where("verification_code = ?", code).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u UserRepository) GetById(id uuid.UUID) (*entity.User, error) {
 	var user *entity.User
 
 	err := u.db.Debug().Where("id = ?", id).First(&user).Error
@@ -52,4 +63,13 @@ func (u UserRepository) GetById(id string) (*entity.User, error) {
 	}
 
 	return user, nil
+}
+
+func (u UserRepository) Update(user *entity.User, req model.UserUpdate) error {
+	err := u.db.Debug().Model(&user).Updates(req).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
