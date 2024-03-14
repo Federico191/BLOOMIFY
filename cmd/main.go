fb737f8c5f82940bbd7c557dd3b091b7bc300fd1
Federico191 <federicoberto708@gmail.com>
2024-03-15 03:49:35 +0700

    refactor: add env check in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	supabasestorageuploader "github.com/adityarizkyramadhan/supabase-storage-uploader"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 	"projectIntern/internal/delivery/handler/rest"
 	"projectIntern/internal/delivery/middleware"
 	"projectIntern/internal/delivery/routes"
@@ -17,6 +19,12 @@ import (
 )
 
 func main() {
+	err := godotenv.Load("../app.env")
+	envi := os.Getenv("ENV")
+	if err != nil && envi == "" {
+		log.Fatalf("cannot load env:%v", err)
+	}
+
 	env, err := config.NewEnv("../")
 	if err != nil && env == nil {
 		log.Fatalf("cannot load env: %v", err)
