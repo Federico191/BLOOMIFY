a9c831270aa102ecdd0224d0973093fc7117d3cd
Federico191 <federicoberto708@gmail.com>
2024-03-20 12:05:56 +0700

    refactor : modify review into 2 type, TreatmentReview and DoctorReview

--- internal/entity/review.go
+++ internal/entity/review.go
@@ -5,13 +5,25 @@ import (
 	"time"
 )
 
-type Review struct {
+type TreatmentReview struct {
 	ID        uint      `gorm:"primaryKey;autoIncrement"`
 	UserID    uuid.UUID `gorm:"size:36"`
 	User      User      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	ServiceID uint      `gorm:"foreignKey:ServiceID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	Rating    int       `gorm:"not null"`
-	Review    string    `gorm:"not null"`
+	Review    string    `gorm:"size:255;not null"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+}
+
+type DoctorReview struct {
+	ID        uint      `gorm:"primaryKey;autoIncrement"`
+	UserID    uuid.UUID `gorm:"size:36"`
+	User      User      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	DoctorID  uuid.UUID `gorm:"size:36"`
+	Doctor    Doctor    `gorm:"foreignKey:DoctorID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Rating    int       `gorm:"not null"`
+	Review    string    `gorm:"size:255;not null"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
