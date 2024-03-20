54e3f4b0b289c56c9fd3585aadd4d635729a0587
Federico191 <federicoberto708@gmail.com>
2024-03-20 11:53:14 +0700

    feat : modify booking model

--- model/booking.go
+++ model/booking.go
@@ -5,12 +5,49 @@ import (
 	"time"
 )
 
-type BookingRequest struct {
-	ID          string    `json:"id"`
-	UserId      uuid.UUID `json:"user_id"`
-	ServiceId   uint      `json:"service_id"`
-	Day         time.Time `json:"day"`
-	Time        time.Time `json:"time"`
-	PaymentId   uint      `json:"payment_id"`
-	GrossAmount int       `json:"gross_amount"`
+type BookingDoctorRequest struct {
+	DoctorId      uuid.UUID `json:"doctor_id"`
+	Day           string    `json:"day"`
+	Time          string    `json:"time"`
+	PaymentMethod string    `json:"payment_method"`
+}
+
+type BookingTreatmentRequest struct {
+	ServiceId     uint   `json:"service_id"`
+	Day           string `json:"day"`
+	Time          string `json:"time"`
+	PaymentMethod string `json:"payment_method"`
+}
+
+type BookingTreatmentResponse struct {
+	BookingId     string    `json:"booking_id"`
+	TransactionId string    `json:"transaction_id"`
+	UserFullName  string    `json:"user_full_name"`
+	UserEmail     string    `json:"user_email"`
+	PlaceName     string    `json:"place_name"`
+	ServiceName   string    `json:"service_name"`
+	PlaceAddress  string    `json:"service_address"`
+	PaymentMethod string    `json:"payment_method"`
+	PaymentCode   string    `json:"payment_code"`
+	GrossAmount   int64     `json:"gross_amount"`
+	Status        string    `json:"status"`
+	BookAt        time.Time `json:"book_at"`
+}
+
+type BookingDoctorResponse struct {
+	BookingId     string    `json:"booking_id"`
+	TransactionId string    `json:"transaction_id"`
+	UserFullName  string    `json:"user_full_name"`
+	UserEmail     string    `json:"user_email"`
+	DoctorName    string    `json:"doctor_name"`
+	Profession    string    `json:"profession"`
+	PaymentMethod string    `json:"payment_method"`
+	PaymentCode   string    `json:"payment_code"`
+	GrossAmount   int64     `json:"gross_amount"`
+	Status        string    `json:"status"`
+	BookAt        time.Time `json:"book_at"`
+}
+
+type BookingStatus struct {
+	TransactionId string `json:"transaction_id"`
 }
