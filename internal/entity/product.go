2454d69b123682d06d2a73c1094dec0e66112358
Federico191 <federicoberto708@gmail.com>
2024-03-22 05:33:42 +0700

    refactor : add new column price, desc, rating in product entity

--- internal/entity/product.go
+++ internal/entity/product.go
@@ -5,11 +5,14 @@ import (
 )
 
 type Product struct {
-	ID        uint      `gorm:"primaryKey;autoIncrement"`
-	Name      string    `gorm:"size:255;not null"`
-	ProblemId uint      `gorm:"foreignKey:ID"`
-	Problem   Problem   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	PhotoLink string    `gorm:"size:255"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+	ID          uint      `gorm:"primaryKey;autoIncrement"`
+	Name        string    `gorm:"size:255;not null"`
+	ProblemId   uint      `gorm:"foreignKey:ID"`
+	Problem     Problem   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Price       int       `gorm:"not null"`
+	Description string    `gorm:"size:100;not null"`
+	Rating      float64   `gorm:"not null"`
+	PhotoLink   string    `gorm:"size:255"`
+	CreatedAt   time.Time `gorm:"autoCreateTime"`
+	UpdatedAt   time.Time `gorm:"autoUpdateTime"`
 }
