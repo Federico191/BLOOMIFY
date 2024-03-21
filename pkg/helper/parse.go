4a46bb5831d26ff600d658b13782ebcb0dac548e
Federico191 <federicoberto708@gmail.com>
2024-03-22 05:32:36 +0700

    refactor : modify dateFormat

--- pkg/helper/parse.go
+++ pkg/helper/parse.go
@@ -5,22 +5,17 @@ import (
 )
 
 func ParseDateTime(dayStr, timeStr string) (time.Time, error) {
-	// Format string untuk tanggal
-	dateFormat := "2006-01-02" // Format: yyyy-mm-dd
+	dateFormat := "02-01-2006"
 
-	// Format string untuk waktu
-	timeFormat := "15.04" // Format: HH:mm (24 jam)
+	timeFormat := "15.04"
 
-	// Gabungkan string tanggal dan waktu
 	dateTimeStr := dayStr + " " + timeStr
 
-	// Set zona waktu yang diharapkan
-	loc, err := time.LoadLocation("Asia/Jakarta") // Atur sesuai dengan zona waktu yang diinginkan
+	loc, err := time.LoadLocation("Asia/Jakarta")
 	if err != nil {
 		return time.Time{}, err
 	}
 
-	// Parsing string menjadi time.Time dengan zona waktu yang diharapkan
 	dateTime, err := time.ParseInLocation(dateFormat+" "+timeFormat, dateTimeStr, loc)
 	if err != nil {
 		return time.Time{}, err
